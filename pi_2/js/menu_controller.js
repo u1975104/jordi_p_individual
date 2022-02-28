97a45c725795f00a9331f920ba6b2aa14d4ab558
jordi bertomeu primo <99675370+u1975104@users.noreply.github.com>
2022-02-28 18:47:24 +0100

    Commit de la pràctica 2
    Primer commit de la pt2

---
+++ pi_2/js/menu_controller.js
@@ -0,0 +1,15 @@
+function start_game(){
+	name = prompt("User name");
+	loadpage("./html/game.html");
+}
+
+function exit (){
+	window.location.href = '../index.html/';
+}
+
+function options(){
+	// TODO: Open options menu
+	console.log("Options menu button");
+}
+
+
